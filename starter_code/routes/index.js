edd82762eb492aeb0abd4f2909aae84f2c5f5180
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-02 12:51:37 +0100

    For my love]

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/movies', (req, res, next) => {
+  res.render('movies');
+});
+
+
 
 module.exports = router;
